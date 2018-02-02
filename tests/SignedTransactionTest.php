57b3c36a5373d421746bd8b88a496b3255fd62ed
TiMESPLiNTER <github@timesplinter.ch>
2018-02-02 19:59:45 +0100

    Add dummy test

---
+++ tests/SignedTransactionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Timesplinter\TxBlockchain\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Timesplinter\TxBlockchain\SignedTransaction
+ */
+class SignedTransactionTest extends TestCase
+{
+    public function test()
+    {
+        // @todo
+    }
+}
