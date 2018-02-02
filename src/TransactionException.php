69180b40c8c7c698c305e9061dd4c431136dbfaf
TiMESPLiNTER <github@timesplinter.ch>
2018-02-02 15:51:54 +0100

    inital commit

---
+++ src/TransactionException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Timesplinter\TxBlockchain;
+
+abstract class TransactionException extends \Exception
+{
+
+}
---
+++ src/TransactionSignatureException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Timesplinter\TxBlockchain;
+
+class TransactionSignatureException extends TransactionException
+{
+
+}
